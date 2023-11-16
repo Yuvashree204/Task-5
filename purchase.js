9f4d76d5cec1a9fe5b47b296a71649a5465bea0d
Yuvashree204 <133845863+Yuvashree204@users.noreply.github.com>
2023-11-16 21:17:19 +0530

    Add files via upload

--- purchase.js
+++ purchase.js
@@ -1,43 +1,32 @@
-const Purchase = require("../models/purchase");
-const purchaseStock = require("./purchaseStock");
+const mongoose = require("mongoose");
 
-// Add Purchase Details
-const addPurchase = (req, res) => {
-  const addPurchaseDetails = new Purchase({
-    userID: req.body.userID,
-    ProductID: req.body.productID,
-    QuantityPurchased: req.body.quantityPurchased,
-    PurchaseDate: req.body.purchaseDate,
-    TotalPurchaseAmount: req.body.totalPurchaseAmount,
-  });
+const PurchaseSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    ProductID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+      required: true,
+    },
+    QuantityPurchased: {
+      type: Number,
+      required: true,
+    },
+    PurchaseDate: {
+      type: String,
+      required: true,
+    },
+    TotalPurchaseAmount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-  addPurchaseDetails
-    .save()
-    .then((result) => {
-      purchaseStock(req.body.productID, req.body.quantityPurchased);
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      res.status(402).send(err);
-    });
-};
-
-// Get All Purchase Data
-const getPurchaseData = async (req, res) => {
-  const findAllPurchaseData = await Purchase.find({"userID": req.params.userID})
-    .sort({ _id: -1 })
-    .populate("ProductID"); // -1 for descending order
-  res.json(findAllPurchaseData);
-};
-
-// Get total purchase amount
-const getTotalPurchaseAmount = async (req, res) => {
-  let totalPurchaseAmount = 0;
-  const purchaseData = await Purchase.find({"userID": req.params.userID});
-  purchaseData.forEach((purchase) => {
-    totalPurchaseAmount += purchase.TotalPurchaseAmount;
-  });
-  res.json({ totalPurchaseAmount });
-};
-
-module.exports = { addPurchase, getPurchaseData, getTotalPurchaseAmount };
+const Purchase = mongoose.model("purchase", PurchaseSchema);
+module.exports = Purchase;
--- store.js
+++ store.js
@@ -1,29 +1,35 @@
-const Store = require("../models/store");
+const mongoose = require("mongoose");
 
-// Add Store
-const addStore = async (req, res) => {
-    console.log(req.body)
-  const addStore = await new Store({
-    userID : req.body.userId,
-    name: req.body.name,
-    category: req.body.category,
-    address: req.body.address,
-    city: req.body.city,
-    image: req.body.image
-  });
+const StoreSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-  addStore.save().then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      res.status(402).send(err);
-    });
-};
-
-// Get All Stores
-const getAllStores = async (req, res) => {
-  const findAllStores = await Store.find({"userID": req.params.userID}).sort({ _id: -1 }); // -1 for descending;
-  res.json(findAllStores);
-};
-
-module.exports = { addStore, getAllStores };
+const Store = mongoose.model("store", StoreSchema);
+module.exports = Store;
